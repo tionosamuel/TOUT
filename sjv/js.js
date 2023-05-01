f0debde6caf5032cbd7044c28fedfcb0725a1421
T <tionosamuel4@gmail.com>
2023-05-01 19:52:42 +0200

    premier commit

---
+++ sjv/js.js
@@ -0,0 +1,113 @@
+// const test = document.querySelector("nav");
+// // console.log(test);
+// window.addEventListener('scroll',()=>{
+//     console.log(window.scrollY);
+//     if(window.scrollY>330){
+//     test.style.top=0;
+
+
+//     }else {
+//         test.style.top="-80px";
+
+//     }
+
+// })
+
+// const liste=document.getElementById("liste");
+// console.log(liste);
+// // etape 2:creer un element 
+// const element=document.createElement("li")
+// console.log(element);
+// // creation de la classe item et l'ajout
+// element.classList.add("item");
+// // ajouter un texte
+// element.innerText="nouveau element ajouter";
+// const ajout=liste.appendChild(element);
+// // console.log(ajout);
+// const button= document.querySelector("#submit");
+// const stocke= button.addEventListener("click", addEvent);
+// function addEvent() {
+//     const liste= document.getElementById("liste");
+//     const element = document.createElement("li");
+//     element.classList.add("item");
+//     element.innerText="nouveau element ajouter";
+//     const ajout = liste.appendChild(element);
+// }
+
+
+
+// let monAge = 26;
+// let telephone =77538448;
+// let point=10;
+// let annee =2020;
+// const code_pin = 1234;
+// console.log(code_Pin);
+// code_pin=4321; 
+// annee=2021
+
+// console.log(annee)
+
+// age=27
+// console.log (age,telephone)
+
+// console.log("salut les professionnels");
+// console.log('salut les pros des pros');
+// let prenom=("tionoSamuel");
+// let nom=('frero');
+// console.log("prenom");
+// nom=('kiénon');
+// console.log("nom");
+// console.log(nom,prenom,surnom);
+// surnom="gare";
+// let nom_complet=nom +" " + prenom +" samba";
+// console.log(nom_complet);
+// let concat = 'je me nomme ${prenom} et aussi ${surnom}';
+// console.log('nom,surnom');
+// let nom = ('samuel');
+// console.log(nom.indexOf ('a'))
+// console.log(nom.lasteIndexOf('a'));
+// console.log (10%3);
+// A = pi * R2
+// P = 2*pi*R
+// AR = L*l
+//   let rayon = 10;
+//   let pi = 3.14
+//   let longueur = 45;
+
+//   let largeur = 10;
+
+//   let Surface_circcle = pi * rayon**2;
+//   let surface_rect    = longueur *  largeur;
+//   let perimettre       = 2 * pi * rayon;
+
+//   let result = `mon cercle a pour perimetre: ${ perimettre}  et pour surface: ${ Surface_circcle} et mon rectangle a pour surface: ${surface_rect}.`;
+
+//   console.log(result);
+
+// // ++ --
+//  let likes = 10;
+//   likes /= 2;  
+//  console.log(likes);
+
+// array = tableau
+
+// let fruits = ['banane', 'ananas', 'coco', 'choux', 'papayes'];
+
+// fruits[4] = 'cerise';
+
+// console.log(fruits);
+
+// console.log(fruits['2']);
+// console.log(fruits['3']);
+// console.log(fruits['4']);
+
+// let array_aleatoire = ['tiono', 'choux', 'papaye', 'coco', '25', 'false', 'good'];
+// console.log(array_aleatoire);
+
+// console.log(fruits.length);
+
+let fruits = ['banane', 'ananas', 'coco', 'choux', 'papayes', 'mangues'];
+console.log (fruits.join('-'))
+
+
+
